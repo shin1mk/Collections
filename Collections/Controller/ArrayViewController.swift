c08c401de9b13bcd83dcf5e0059d4d4aed19ae06
MK1 <shin.m.dp@gmail.com>
2023-07-08 22:34:28 +0300

    added func generateIntArray

--- Collections/Controller/ArrayViewController.swift
+++ Collections/Controller/ArrayViewController.swift
@@ -3,7 +3,6 @@
 //  Collections
 //
 //  Created by SHIN MIKHAIL on 07.07.2023.
-//
 
 import UIKit
 import SnapKit
@@ -32,6 +31,7 @@ final class ArrayViewController: UIViewController {
         let tableView = UITableView()
         return tableView
     }()
+    
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,29 +75,4 @@ final class ArrayViewController: UIViewController {
     @objc private func backButtonTapped() {
         navigationController?.popViewController(animated: true)
     }
-} // array_controller
-
-extension ArrayViewController: UITableViewDelegate, UITableViewDataSource {
-    // Количество строк в таблице
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    // Установите желаемую высоту строки
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
-    // содержимое строки
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Create Int array with 10_000_000 elements"
-        cell.textLabel?.textColor = .black
-        cell.textLabel?.textAlignment = .center
-        cell.backgroundColor = .white
-        // Remove left and right margins
-        cell.separatorInset = UIEdgeInsets.zero
-        cell.layoutMargins = UIEdgeInsets.zero
-        
-        return cell
-    }
 }
-
