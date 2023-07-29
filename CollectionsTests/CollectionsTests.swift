1351b550c576ca76e0d9be2bae814d049908948e
MK1 <shin.m.dp@gmail.com>
2023-07-29 13:18:35 +0300

    unit test5

--- CollectionsTests/CollectionsTests.swift
+++ CollectionsTests/CollectionsTests.swift
@@ -8,29 +8,100 @@
 import XCTest
 @testable import Collections
 
-final class CollectionsTests: XCTestCase {
+final class ArrayOperationsTests: XCTestCase {
+    var arrayOperations: ArrayOperations!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        arrayOperations = ArrayOperations()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        arrayOperations = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGenerateIntArray() {
+        let expectation = XCTestExpectation(description: "Generate int array")
+        arrayOperations.generateIntArray { timeInSeconds in
+            XCTAssertGreaterThan(timeInSeconds, 0, "Generating int array should take some time")
+            expectation.fulfill()
         }
     }
+}
 
+final class SetOperationsTests: XCTestCase {
+    var setOperations: SetOperations!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        setOperations = SetOperations()
+    }
+
+    override func tearDownWithError() throws {
+        setOperations = nil
+        try super.tearDownWithError()
+    }
+
+    // Test findMatchingCharacters
+    func testSetMatchChars() throws {
+        let text1 = "Temp"
+        let text2 = "amper"
+        let expectedResult = "emp"
+        
+        let setOperationsResult = setOperations.findMatchingCharacters(textFull: text1, textCharacters: text2)
+        
+        let expectedSet = Set(expectedResult)
+        let resultSet = Set(setOperationsResult)
+        XCTAssertEqual(expectedSet, resultSet)
+    }
+
+    // Test findNonMatchingCharacters
+    func testSetNotMatchChars() throws {
+        let text1 = "Temp"
+        let text2 = "amper"
+        let expectedResult = "Tar"
+        
+        let setOperationsResult = setOperations.findNonMatchingCharacters(textFull: text1, textCharacters: text2)
+        
+        let expectedSet = Set(expectedResult)
+        let resultSet = Set(setOperationsResult)
+        XCTAssertEqual(expectedSet, resultSet)
+    }
+
+    // Test findUniqueCharacters
+    func testSetUniqueChars() throws {
+        let text1 = "Temp"
+        let text2 = "amper"
+        let expectedResult = "T"
+        
+        let setOperationsResult = setOperations.findUniqueCharacters(textFull: text1, textCharacters: text2)
+        
+        XCTAssertEqual(expectedResult, setOperationsResult)
+    }
+}
+
+final class DictionaryOperationsTests: XCTestCase {
+    var dictionaryOperations: DictionaryOperations!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        dictionaryOperations = DictionaryOperations()
+        dictionaryOperations.generateDictionaryArray()
+    }
+
+    override func tearDownWithError() throws {
+        dictionaryOperations = nil
+        try super.tearDownWithError()
+    }
+
+    func testFindNotExistingElenemtInDictionary() throws {
+        let expectation = self.expectation(description: "Find Not Existing Element in Dictionary")
+
+        dictionaryOperations.findNotExistingElenemtInDictionary { (time, flag) in
+            XCTAssertEqual(flag, 0, "The flag should be 0 as the element does not exist")
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10.0, handler: nil)
+    }
 }
